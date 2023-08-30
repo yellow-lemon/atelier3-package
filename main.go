3f109f502e7e210bae85ffbdfa865f804fb70f22
Lemon <85191885+Yellow-Lemon@users.noreply.github.com>
2023-08-30 14:01:17 -0400

    finished

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-fun test() {
-	
+import (
+	"math/rand"
+)
+
+func ArrayGenerate(size int) []int {
+	var array []int
+	for i := 0; i <= size; i++ {
+		array = append(array, rand.Intn(10000))
+	}
+
+	return array
 }
